82bbe03dc29fd1c5bd8e4da13fc4caf6643dc588
Rane M Kaseman <raykman@comcast.net>
2018-09-13 00:36:43 -0600

    0036

--- config/orm.js
+++ config/orm.js
@@ -3,12 +3,24 @@ var connection = require("./connection.js");
 
 
 var orm = {
-    selectWhere: function(burgerName, devoured, anonFunction) {
+    select: function(selCol, selTable, callBack) {
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [selCol, selTable], function(error, result) {
+            if (error) throw error;
+            console.log(result);
+            callBack(result);
+        });
+    },
+    selectWhere: function (selTable, selCol, selColVal, callBack) {
         var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-        connection.query(queryString, [table])
+        connection.query(queryString, [selTable, selCol, selColVal], function (error, result) {
+            if (error) throw error;
+            console.log(result);
+            callBack(result);
+        });
     }
-}
+};
 
 
-module.exports = connection;
+module.exports = orm;
 
--- server.js
+++ server.js
@@ -1,30 +1,8 @@
 
-var express = require("express");
-var bodyParser = require("body-parser");
+var orm = require("./config/orm.js");
 
 
-// instance of express app
-var app = express();
-
-
-// variable port selection
-var PORT = process.env.PORT || 8080;
-
-
-// access to js/css
-app.use(express.static("app/public"));
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
-
-
-// start the server
-app.listen(PORT, function () {
-    console.log("server at http://localhost:" + PORT);
+orm.select("burgers", "burger_name", "devoured", function(result) {
+    console.log(result);
 });
 
