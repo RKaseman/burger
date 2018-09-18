72d462c7f655ae2b6bcc509afdc38ab96011c8d7
Rane M Kaseman <raykman@comcast.net>
2018-09-17 23:42:32 -0600

    2342

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,21 @@
 
 // mySQL connection
 var mysql = require("mysql");
+var connection;
 
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
+};
 
 
-connection.connect(function(error) {
-    if (error) {
-        console.log("Connection error" + error.stack);
-        return;
-    }
-    console.log("Connection id " + connection.threadId);
-});
-
-// to orm.js
+connection.connect();
 module.exports = connection;
 
